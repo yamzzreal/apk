fd8a883fd08d798030e8e0d0a1cd9fd855aac7d4
Payment Yamzz Real <ryanrmdn207@gmail.com>
2025-09-27 15:26:48 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,7 @@
+//credit By Limzzzhama 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "silentt.yamzzoffc.my.id", // Ubah jadi Domain panel Mu !!!
+  port: "2023" // Ubah Jadi Port Panel Mu !!!
+};
